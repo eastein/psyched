e9067f07e55ebf4e217a03355ce1abf69920c73f
Eric Stein <toba@des.truct.org>
2007-12-14 10:26:18 +0000

    finished splitting callbacks, all schedule info saves to database now

--- psyched_validate.py
+++ psyched_validate.py
@@ -12,5 +12,5 @@ def check_time(s) :
 	return (_reg.match(s) != None)
 
 def check_interval(s) :
-	_reg = re.compile('^([1-9]{1}[0-9]*:[0-5][0-9]|[0-9]+)$')
+	_reg = re.compile('^([0-9]*:[0-5][0-9]|[0-9]+)$')
 	return (_reg.match(s) != None)
