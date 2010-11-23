f76a91a6b17d0672ef6861ad8399aa4a51677554
Eric Stein <toba@des.truct.org>
2010-11-23 05:06:09 +0000

    remove the today button - it crowds the interface on most platforms. stop requiring colons and as picky formatting for times and intervals.

--- psyched_validate.py
+++ psyched_validate.py
@@ -10,9 +10,9 @@ $Id$
 import re
 
 def check_time(s) :
-	_reg = re.compile('^(2[0-3]|1[0-9]|0{0,1}[0-9]):[0-5][0-9]$')
+	_reg = re.compile('^(2[0-3]|1[0-9]|0{0,1}[0-9])(:|)[0-5][0-9]$')
 	return (_reg.match(s) != None)
 
 def check_interval(s) :
-	_reg = re.compile('^([0-9]*:[0-5][0-9]|[0-9]+)$')
+	_reg = re.compile('^([0-9]*(:|)[0-5][0-9]|[0-9])$')
 	return (_reg.match(s) != None)
